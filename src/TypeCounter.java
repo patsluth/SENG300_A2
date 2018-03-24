7ed9b0da7669fd5604ded97bac2ec34d4d49fa9f
Pat Sluth <pat.sluth@gmail.com>
2018-03-24 16:28:41 -0600

    update

---
+++ src/TypeCounter.java
@@ -0,0 +1,112 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Scanner;
+
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.SimpleName;
+
+public class TypeCounter 
+{
+	private String directoryPath;
+	private String typeName = null;
+	
+	private ASTParser parser = null;
+	private int declarationCount = 0;
+	private int referenceCount = 0;
+	
+	
+	
+	
+	
+	public TypeCounter(String directoryPath, String typeName)
+	{
+		File directoryFile = new File(directoryPath);
+		if (!directoryFile.isDirectory()) {
+			System.out.println("Invalid directory.");
+			return;
+		}
+		
+		this.parser = ASTParser.newParser(AST.JLS8);
+		this.directoryPath = directoryPath;
+		this.typeName = typeName;
+	}
+	
+	public void run()
+	{
+		try {
+			Files.walk(Paths.get(directoryPath))
+			.filter(Files::isRegularFile)
+			.forEach( (filePath) -> {
+				this.parse(filePath);
+	        });
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} 
+	}
+	
+	private void parse(Path filePath)
+	{
+		File file = filePath.toFile();
+		
+		if (file.isDirectory()) { return; }
+		
+		String filename = file.getName();
+		String[] parts = filename.split("\\.");
+		String extension = parts[parts.length - 1]; 
+		
+		if (!extension.toLowerCase().equals("java") && !extension.toLowerCase().equals("jar")) { return; }
+		
+		try {	
+			String source = readFile(file);
+			
+			System.out.println("Reading file: " + file.getPath());
+
+			this.parser.setSource(source.toCharArray());
+			
+			Map options = JavaCore.getOptions();
+			JavaCore.setComplianceOptions(JavaCore.VERSION_1_5, options); // update compiler version to 1.5 to recognize enum declarations
+			this.parser.setCompilerOptions(options);
+
+			CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+			
+			cu.accept(new ASTVisitor() {
+				@Override public boolean visit(SimpleName node) 
+				{
+					System.out.println(node.getFullyQualifiedName());
+					
+					return super.visit(node);
+				}
+			});
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("Failed to read file: " + file.getPath());
+		}
+	}
+
+	private String readFile(File file) throws FileNotFoundException
+	{
+	    Scanner scanner = new Scanner(file);
+	    String source = "";
+	
+	    while (scanner.hasNextLine()) {
+	    	source += scanner.nextLine() + "\n";
+	    }
+	    
+	    scanner.close();
+	
+	    return source;
+	}
+}
+
+
+
+
